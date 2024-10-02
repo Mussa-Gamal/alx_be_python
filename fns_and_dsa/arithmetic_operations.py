2fecc5528d651ce346f2127a83929ac60b97a0df
Mussa Gamal <mussagamal@gmail.com>
2024-10-02 17:14:20 +0300

    added fns_and_dsa directory along with main.py and arithmetic_operations.py

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,15 @@
+def perform_operation(num1, num2, operation):
+  match operation:
+    case '+':
+      result = num1 + num2
+    case '-':
+      result = num1 - num2
+    case '*':
+      result = num1 * num2
+    case '/':
+      if num2 == 0:
+        return print('Cannot divide by zero.')
+      else:
+        result = num1 / num2
+
+  return result
