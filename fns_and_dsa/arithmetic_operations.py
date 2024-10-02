3a62ef2e19eef1a45ba1ab4fb8e13bedc366097e
Mussa Gamal <mussagamal@gmail.com>
2024-10-02 17:19:53 +0300

    arithmetic_operation.py fixed #1

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,12 +1,12 @@
 def perform_operation(num1, num2, operation):
   match operation:
-    case '+':
+    case 'add':
       result = num1 + num2
-    case '-':
+    case 'subtract':
       result = num1 - num2
-    case '*':
+    case 'multiply':
       result = num1 * num2
-    case '/':
+    case 'divide':
       if num2 == 0:
         return print('Cannot divide by zero.')
       else:
